714671c33b8b467199d4ce0ea32a4ae4878a5d23
Tony <itony215@gmail.com>
2022-07-21 17:24:28 +0800

    弱勢股Line通知

---
+++ getTarget.py
@@ -0,0 +1,32 @@
+import os
+import json
+import requests
+
+def lineNotify(token, msg):
+    url = "https://notify-api.line.me/api/notify"
+    headers = {
+        "Authorization": "Bearer " + token
+    }
+   
+    payload = {'message': msg}
+    r = requests.post(url, headers = headers, params = payload)
+    return r.status_code
+
+with os.scandir('/home/pineapple/Documents/stock/day/') as it:
+    for entry in it:
+        notice = ''
+        if entry.name.endswith(".json") and entry.is_file():
+            #print('2222',entry.name, entry.path)
+            with open(entry.path,'r+') as json_file:
+                old_data = json.load(json_file)
+                #print(old_data)
+                openPrice = old_data['openPrice'][-1]
+                #print('error', openPrice,entry.name)
+                inMarket = old_data['inMarket'][-1]
+                outMarket = old_data['outMarket'][-1]
+                avgPrice = old_data['avgPrice'][-1]
+    
+                if(avgPrice>openPrice and inMarket>outMarket):
+                    print(old_data["symbolName"],old_data["stockId"])
+                    notice = '弱勢股: '+old_data["symbolName"]+old_data["stockId"]
+                    lineNotify('X57Kb4EhV6073WKCE9UU2eT3IBvxmY44LPtmdUwwS8O', notice)
