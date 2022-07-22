70b6f2bd28eddaf677a6c7aeba709e381dcc4a52
Tony <itony215@gmail.com>
2022-07-22 10:38:06 +0800

    Fix tx format bug

--- tx.py
+++ tx.py
@@ -8,7 +8,7 @@ today = date.today()
 datestr = today.strftime("%Y%m%d")
 
 def tx_crawler(): 
-    filepath = "/home/pineapple/Documents/stock/day/"+datestr+"_tx.json"
+    filepath = "./day/"+datestr+"_tx.json"
     try:
         with open(filepath,'r+') as json_file:
             old_data = json.load(json_file)
@@ -24,13 +24,17 @@ def tx_crawler():
 
     res = r.get("https://tw.quote.finance.yahoo.net/quote/q?type=tick&perd=1m&mkt=10&sym=%23001")
     #print(res.text[5:-2])
-    data = json.loads(res.text[5:-2])
+    tick_index = res.text.rfind('tick')
+    #print(res.text[index-1:-3])
+    data_string = '{'+res.text[tick_index-1:-2]
+    data = json.loads(data_string)
+    
     #script_text = soup.find('tick').get_text()
     #print(soup.tick)
-    if(len(timestamp)<len(data['tick'])):
-        timestamp = [x['t'] for x in data['tick']]
-        price = [x['p'] for x in data['tick']]
-        volumn = [x['v'] for x in data['tick']]
+    if(len(timestamp)<len(data)):
+        timestamp = [x['t'] for x in data]
+        price = [x['p'] for x in data]
+        volumn = [x['v'] for x in data]
         jsonObject = {"timestamp": timestamp, "price": price, "volumn":volumn}
         file = open(filepath, "w")
         json.dump(jsonObject, file)
@@ -44,7 +48,7 @@ def tx_crawler():
     
     #display(df)
     
-    start_time = datetime.strptime(str(time.date())+'08:55', '%Y-%m-%d%H:%M')
+    start_time = datetime.strptime(str(time.date())+'08:30', '%Y-%m-%d%H:%M')
     end_time =  datetime.strptime(str(time.date())+'13:35', '%Y-%m-%d%H:%M')
     
     if time >= start_time and time <= end_time:
