9361497b5f724b925a81ed1213e6039505940bd3
Tony <itony215@gmail.com>
2022-07-26 11:16:34 +0800

    alert with roe

---
+++ getTarget_roe.py
@@ -0,0 +1,40 @@
+import os
+import json
+import requests
+
+def lineNotify(token, msg):
+    url = "https://notify-api.line.me/api/notify"
+    headers = {
+        "Authorization": "Bearer " + token
+    }
+   
+    payload = {'message': msg}
+    r = requests.post(url, headers = headers, params = payload)
+    return r.status_code
+notice = '弱勢股:\n'
+with open('./stock_roe.json','r+') as roe_file:
+    roe = json.load(roe_file)
+with os.scandir('/home/pineapple/Documents/stock/day/') as it:
+    for entry in it:
+        #print(entry)
+        if entry.name.endswith(".json") and entry.is_file():
+            #print('2222',entry.name, entry.path)
+            with open(entry.path,'r+') as json_file:
+                old_data = json.load(json_file)
+                #print(old_data)
+                openPrice = old_data['openPrice'][-1]
+                #print('error', openPrice,entry.name)
+                inMarket = old_data['inMarket'][-1]
+                outMarket = old_data['outMarket'][-1]
+                avgPrice = old_data['avgPrice'][-1]
+    
+                if(avgPrice>openPrice and inMarket>outMarket):
+                    if old_data["stockId"] in roe:
+                        if(openPrice>roe[old_data["stockId"]]):
+                            print(old_data["symbolName"],old_data["stockId"])
+                            notice += old_data["symbolName"]+old_data["stockId"] + '\n'
+                    else:
+                        print(old_data["symbolName"],old_data["stockId"])
+                        notice += old_data["symbolName"]+old_data["stockId"] + '\n'    
+
+lineNotify('X57Kb4EhV6073WKCE9UU2eT3IBvxmY44LPtmdUwwS8O', notice)
