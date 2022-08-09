448272cb31ae80ea49fd2143fe1e001c2672a17a
Tony <itony215@gmail.com>
2022-08-09 15:48:50 +0800

    alert when stock over top bband

---
+++ too_high_stock.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import requests
+import datetime
+import time
+from io import StringIO
+from bs4 import BeautifulSoup
+import pickle
+import os
+import json
+from datetime import date
+import os.path as path
+
+high = pd.read_pickle("/home/pineapple/Documents/stock/crawler/history/最高價.pkl")
+low = pd.read_pickle("/home/pineapple/Documents/stock/crawler/history/最低價.pkl")
+start = pd.read_pickle("/home/pineapple/Documents/stock/crawler/history/開盤價.pkl")
+end = pd.read_pickle("/home/pineapple/Documents/stock/crawler/history/收盤價.pkl")
+volumn = pd.read_pickle("/home/pineapple/Documents/stock/crawler/history/成交股數.pkl")
+
+for s in (high.columns):
+    try:
+        id = str(s)
+        result = pd.concat([start[id],high[id],low[id],end[id],round(volumn[id]/1000,2)], axis=1, keys=["開盤價", "最高價", "最低價", "收盤價","量"])
+        result['漲跌幅'] = round(result['收盤價'].pct_change()*100,2)
+        result['Mean_5']= result['收盤價'].rolling(5).mean()
+        result['Std_5']= result['收盤價'].rolling(5).std()
+        result['BBand_top']= result['Mean_5']+result['Std_5']*0.895
+        result['BBand_down']= result['Mean_5']-result['Std_5']*0.895
+        result['over_top']= result.apply(lambda x : 1 if x['收盤價'] >= x['BBand_top'] else 0, axis=1)
+        #result['over_top'] = result['over_top'].astype(int)
+        result['count'] = result['over_top'].cumsum()-result['over_top'].cumsum().where(result['over_top']==0).ffill().fillna(0).astype(int)
+        #result['count'] = result['over_top'].shift(1) + result['over_top'] if result['over_top'] > 0 else 0
+        #result['count'] = result['count']+(result['count'].shift(1)>=1).astype(int)
+        #result.loc[result['over_top']<1, 'count'] = 0
+        #result['Fruit Total']= result.iloc[:, -3:-1].sum(axis=1)
+        #result['count2'] = result['count'] + result['over_top']
+        #result['count'] = result.apply(lambda x : x['over_top'].shift(1)+1 if x['over_top'] >= 1 else 0, axis=1)#result['over_top'].shift(1)
+        #print(result['over_top'])
+        #reversed_df = result.iloc[::-1]
+        #reversed_df.to_csv("./個股資料/"+id+".csv",encoding='utf_8_sig')
+        result.to_csv("/home/pineapple/Documents/stock/crawler/stock_over_bband/"+id+".csv",encoding='utf_8_sig')
+    except:
+        pass
---
+++ too_high_stock_alert.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+import requests
+
+def lineNotify(token, msg):
+    url = "https://notify-api.line.me/api/notify"
+    headers = {
+        "Authorization": "Bearer " + token
+    }
+   
+    payload = {'message': msg}
+    #files = {'imageFile': open(picURI, 'rb')}
+    r = requests.post(url, headers = headers, params = payload)
+    return r.status_code
+notice = '過熱第四天找空點:\n'
+stock_list = pd.read_pickle("/home/pineapple/Documents/stock/crawler/stock_list.pkl")
+with os.scandir('/home/pineapple/Documents/stock/crawler/stock_over_bband/') as it:
+    for entry in it:
+        if entry.name.endswith(".csv") and entry.is_file():
+            #print(entry.name, entry.path)
+            df = pd.read_csv(entry.path, index_col="date")
+            if(df.iloc[-1]['count']>2 and df.iloc[-1]['漲跌幅']>1 and df.iloc[-1]['收盤價']>20 and df.iloc[-1]['量']>1000):
+                stock_id = entry.name.split('.')[0]
+                notice += stock_id+stock_list[stock_list.index==stock_id]['證券名稱'].values[0]+': '+str(df.iloc[-1]['收盤價'])+'\n'
+                print(stock_id,stock_list[stock_list.index==stock_id]['證券名稱'].values[0],df.iloc[-1]['收盤價'])
+lineNotify('X57Kb4EhV6073WKCE9UU2eT3IBvxmY44LPtmdUwwS8O', notice)
