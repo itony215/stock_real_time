29a6d4d1aa970a347bf4ce227db933d65bf94e43
Tony <itony215@gmail.com>
2022-11-05 11:03:32 +0800

    add push data to github bash

--- flat_viewer/json_to_streamdata.py
+++ flat_viewer/json_to_streamdata.py
@@ -8,5 +8,5 @@ with os.scandir('/home/pineapple/Documents/stock/crawler/flat_viewer/json/') as 
             with open(entry.path) as f:
                 data = json.load(f)
                 #print(data['stock_data'])
-                with open('/home/pineapple/Documents/stock/crawler/flat_viewer/stream_data/'+entry.name, 'w') as json_file:
+                with open('/home/pineapple/Documents/stock/crawler/flat_viewer/stream_data/flat_viewer_data/json/'+entry.name, 'w') as json_file:
                     json_file.write(str(data['stock_data']))
