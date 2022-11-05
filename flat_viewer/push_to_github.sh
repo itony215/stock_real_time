459f57d4a3987480ad30624907158a5d5e243885
Tony <itony215@gmail.com>
2022-11-05 11:08:55 +0800

    change password

--- flat_viewer/push_to_github.sh
+++ flat_viewer/push_to_github.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/pineapple/Documents/stock/crawler/flat_viewer/stream_data/flat_viewer_data/
 GIT_USERNAME=itony215
-GIT_PASSWORD=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
+GIT_PASSWORD=
 git add .  
 git commit -m $(date +%y%m%d)
 git push https://$GIT_USERNAME:$GIT_PASSWORD@github.com/itony215/flat_viewer_data.git --all
