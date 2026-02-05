fdf4040c091b7306f1b87ef79ab98b2e615fa7f0
tony <tony@keywini.com>
2026-02-05 11:19:08 +0800

    '舊網頁已移除換爬其他券商'

--- update_margin.py
+++ update_margin.py
@@ -3,49 +3,86 @@ import requests
 from datetime import date
 import pandas as pd
 from bs4 import BeautifulSoup
-import datetime
 from tqdm import tqdm
-def transform_ym(date):   #民國轉西元
-        y, m, d = date.split('/')
-        return str(int(y)+1911) + '-' + m + '-' + d
+import os
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+def transform_ym(date_str):   # 民國轉西元
+    y, m, d = date_str.split('/')
+    return str(int(y) + 1911) + '-' + m + '-' + d
 
-stock_list = pd.read_pickle("/home/pineapple/Documents/stock/crawler/history/stock_list.pkl")
-margin = pd.read_pickle("/home/pineapple/Documents/stock/crawler/margin/融資融券.pkl")
+def safe_float(s, default=None, log_info=None):
+    """安全轉 float，若為空字串則回傳 default，並可記錄缺值資訊"""
+    if s is None:
+        if log_info:
+            print(f"[缺值] {log_info} => None")
+        return default
+    s = s.strip().replace(',', '').replace('%', '')
+    if s == '' or s == '-':
+        if log_info:
+            print(f"[缺值] {log_info} => '{s}'")
+        return default
+    try:
+        return float(s)
+    except ValueError:
+        if log_info:
+            print(f"[轉換錯誤] {log_info} => '{s}'")
+        return default
 
-day = datetime.timedelta(days=1)
+# 檔案路徑
+stock_list_path = "/home/keywin/workspace/crawler/history/stock_list.pkl"
+margin_path = "/home/keywin/workspace/crawler/margin/融資融券.pkl"
+
+# 讀取資料
+stock_list = pd.read_pickle(stock_list_path)
+margin = pd.read_pickle(margin_path)
+
 today = date.today()
-#day = today.strftime("%m/%d")
 
+save_counter = 0  # 計數器
 
-#datestr = '2022-08-08'
-for stock_id in tqdm(stock_list.index):
-    if(stock_id in margin.index.levels[0]):
-        #print(stock_id)
+for idx, stock_id in enumerate(tqdm(stock_list.index), 1):
+    if stock_id in margin.index.levels[0]:
         datestr = margin.loc[stock_id].iloc[-1].name.strftime("%Y-%m-%d")
     else:
         datestr = '2023-03-28'
-        #print('eeeeeeeerrr',stock_id)
-    if(datestr<str(today)):
-        #print(stock_id)
-        try:
-            r = requests.get('https://concords.moneydj.com/z/zc/zcn/zcn.djhtm?a='+stock_id+'&c='+datestr+'&d='+str(today))
-            soup = BeautifulSoup(r.text, 'html.parser')
-            data = soup.find_all("td", class_ = ["t3n0","t3n1"])
-            for i in range(1,int(len(data)-4),15):
-                if(transform_ym(data[i].getText())!= datestr):
-                    print('update: ', data[i].getText(), stock_id)
-                    date=transform_ym(data[i].getText())
-                    margin.loc[(stock_id,date),'融資(張)'] = float(data[i+4].getText().replace(',',''))
-                    margin.loc[(stock_id,date),'融資差額(張)'] = float(data[i+5].getText().replace(',',''))
-                    margin.loc[(stock_id,date),'融資使用率'] = float(data[i+7].getText().replace('%',''))
 
-                    margin.loc[(stock_id,date),'融券(張)'] = float(data[i+11].getText().replace(',',''))
-                    margin.loc[(stock_id,date),'融券差額(張)'] = float(data[i+12].getText().replace(',',''))
-                    margin.loc[(stock_id,date),'券資比'] = float(data[i+13].getText().replace('%',''))
-        except:
-            print('error: ', stock_id)
+    if datestr < str(today):
+        try:       #https://jdata.yuanta.com.tw/z/zc/zcn/zcn_1101.djhtm,  jdata.yuanta.com.tw
+            url = f'https://concords.moneydj.com/z/zc/zcn/zcn.djhtm?a={stock_id}&c={datestr}&d={str(today)}'
+            r = requests.get(url, headers=headers)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            data = soup.find_all("td", class_=["t3n0", "t3n1"])
+
+            for i in range(1, int(len(data) - 4), 15):
+                if transform_ym(data[i].getText()) != datestr:
+                    print('update:', data[i].getText(), stock_id, data[i+4].getText(), data[i+11].getText())
+                    date_key = transform_ym(data[i].getText())
+
+                    margin.loc[(stock_id, date_key), '融資(張)'] = safe_float(data[i+4].getText(), 0, f"{stock_id}-{date_key}-融資(張)")
+                    margin.loc[(stock_id, date_key), '融資差額(張)'] = safe_float(data[i+5].getText(), 0, f"{stock_id}-{date_key}-融資差額(張)")
+                    margin.loc[(stock_id, date_key), '融資使用率'] = safe_float(data[i+7].getText(), 0, f"{stock_id}-{date_key}-融資使用率")
+
+                    margin.loc[(stock_id, date_key), '融券(張)'] = safe_float(data[i+11].getText(), 0, f"{stock_id}-{date_key}-融券(張)")
+                    margin.loc[(stock_id, date_key), '融券差額(張)'] = safe_float(data[i+12].getText(), 0, f"{stock_id}-{date_key}-融券差額(張)")
+                    margin.loc[(stock_id, date_key), '券資比'] = safe_float(data[i+13].getText(), 0, f"{stock_id}-{date_key}-券資比")
+
+                    save_counter += 1
+
+                    # 每累積 100 筆儲存一次
+                    if save_counter >= 100:
+                        margin = margin.sort_index()
+                        margin.to_pickle(margin_path)
+                        print(f"已儲存 {save_counter} 筆更新")
+                        save_counter = 0
+
+        except Exception as e:
+            print('error:', stock_id, e)
             continue
-#     else:
-#         print('no update')
-margin = margin.sort_index()
-margin.to_pickle("/home/pineapple/Documents/stock/crawler/margin/融資融券.pkl")
+
+# 迴圈結束後，如果還有沒存的資料，一併儲存
+if save_counter > 0:
+    margin = margin.sort_index()
+    margin.to_pickle(margin_path)
+    print(f"最後儲存 {save_counter} 筆更新")
--- update_three.py
+++ update_three.py
@@ -8,27 +8,31 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 from datetime import datetime
 import os
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
 def transform_ym(date):   #民國轉西元
         y, m, d = date.split('/')
         return str(int(y)+1911) + '-' + m + '-' + d
-stock_list = pd.read_pickle("/home/pineapple/Documents/stock/crawler/history/stock_list.pkl")
-three = pd.read_pickle("/home/pineapple/Documents/stock/crawler/three/三大法人買賣超.pkl")
-stock_total_release = pd.read_pickle("/home/pineapple/Documents/stock/crawler/stock_total_release.pkl")
+stock_list = pd.read_pickle("/home/keywin/workspace/crawler/history/stock_list.pkl")
+three = pd.read_pickle("/home/keywin/workspace/crawler/three/三大法人買賣超.pkl")
+stock_total_release = pd.read_pickle("/home/keywin/workspace/crawler/stock_total_release.pkl")
 today = date.today()
 #day = today.strftime("%m/%d")
 #datestr = today.strftime("%Y-%m-%d")
-#datestr = '2022-08-11'
-for stock_id in tqdm(['1101','6830']):#stock_list.index
+#datestr = '2026-02-04'#
+#for stock_id in tqdm(stock_list.index):#
+for stock_id in stock_list.index:
     if(stock_id in three.index.levels[0]):
         old_date = three.loc[stock_id].iloc[-1].name.strftime("%Y-%m-%d")
     else:
         old_date = str(today)
     if(old_date<=str(today) or three.loc[(stock_id,old_date)].isna().any()):
-        #print(stock_id)
-        #print(stock_id in three.index.levels[0])
+        # print(stock_id)
+        # print(stock_id in three.index.levels[0])
         try:
-            r = requests.get('https://concords.moneydj.com/z/zc/zcl/zcl.djhtm?a='+stock_id+'&c='+old_date+'&d='+str(today))#
-            #https://concords.moneydj.com/z/zc/zcl/zcl_1101.djhtm
+            url = f'https://concords.moneydj.com/z/zc/zcl/zcl.djhtm?a={stock_id}&c={old_date}&d={str(today)}'
+            r = requests.get(url, headers=headers)
             soup = BeautifulSoup(r.text, 'html.parser')
             data = soup.find_all("td", class_ = ["t3n0","t3r1","t3n1"])
             date = transform_ym(data[1].getText())
@@ -71,4 +75,4 @@ for stock_id in tqdm(['1101','6830']):#stock_list.index
 #     else:
 #         print('no update')
 three = three.sort_index()
-three.to_pickle("/home/pineapple/Documents/stock/crawler/three/三大法人買賣超.pkl")
+three.to_pickle("/home/keywin/workspace/crawler/three/三大法人買賣超.pkl")
